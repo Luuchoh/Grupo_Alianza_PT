190a431f4bf218d1607603126a2f7e7ced2cf19f
Luis Hernandez <luxo997@gmail.com>
2025-03-16 14:45:18 -0500

    Add: DbContext and migrations

---
+++ backend/src/Infrastructure/Migrations/20250316174608_InitialCreate.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Producto",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(40)", maxLength: 40, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: false),
+                    Price = table.Column<double>(type: "float", nullable: false),
+                    Stock = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Producto", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Producto");
+        }
+    }
+}
---
+++ backend/src/Infrastructure/Persistence/AplicationDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection.Metadata;
+using System.Xml.Linq;
+
+namespace Infrastructure.Persistence
+{
+    public class AplicationDbContext : DbContext
+    {
+        public virtual DbSet<Product> Product { get; set; }
+
+        public AplicationDbContext() { }
+        public AplicationDbContext(DbContextOptions<AplicationDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Entity<Product>(producto =>
+            {
+                producto.ToTable("Producto");
+                producto.HasKey(p => p.Id);
+                producto.Property(p => p.Id)
+                        .ValueGeneratedOnAdd();
+                producto.Property(p => p.Name).IsRequired().HasMaxLength(40);
+                producto.Property(p => p.Description).IsRequired().HasMaxLength(150);
+                producto.Property(p => p.Price).IsRequired();
+                producto.Property(p => p.Stock).IsRequired();
+            });
+        }
+    }
+}
