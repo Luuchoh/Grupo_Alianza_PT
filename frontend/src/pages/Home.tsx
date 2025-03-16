cd5f6afb2a23b1b17928fd8946acab7af016b14b
Luis Hernández <luxo997@gmail.com>
2025-03-16 17:54:08 -0500

    Add: alias path and test

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import ProductCard from "@components/ProductCard"
+
+const Home = () => {
+  return (
+    <>
+      <div>Home</div>
+      <ProductCard />
+    </>
+  )
+}
+
+export default Home
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import * as path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+
+  resolve: {
+    alias: {
+      "@api" :`${path.resolve(__dirname, "src/api" )}`,
+      "@components" :`${path.resolve(__dirname, "src/components" )}`,
+      "@context" :`${path.resolve(__dirname, "src/context" )}`,
+      "@hooks" :`${path.resolve(__dirname, "src/hooks" )}`,
+      "@pages" :`${path.resolve(__dirname, "src/pages" )}`,
+      "@router" :`${path.resolve(__dirname, "src/router" )}`,
+    }
+  }
 })
