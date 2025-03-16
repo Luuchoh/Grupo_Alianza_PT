e489a9b332e8f27e5456acc5cbc33787292dced3
Luis Hernandez <luxo997@gmail.com>
2025-03-16 14:47:25 -0500

    Add: interfac, controllers and service/repository

---
+++ backend/src/WebAPI/Controllers/ProductController.cs
@@ -0,0 +1,89 @@
+﻿using Application.Interfaces;
+using Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")] 
+    public class ProductoController : ControllerBase 
+    { 
+        private readonly IProduct _repository; 
+        public ProductoController(IProduct repository) 
+        {
+            _repository = repository; 
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                List<Product> productList = await _repository.GetAll();
+                return Ok(productList);
+            } catch (Exception e)
+            {
+                return BadRequest(e);
+            }
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            try
+            {
+                Product product = await _repository.GetByIdAsync(id);
+                if (product == null) return NoContent();
+                return Ok(product);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e);
+            }
+        }
+
+        [HttpPost] 
+        public async Task<IActionResult> Create(Product producto) 
+        {
+            try
+            {
+                await _repository.AddAsync(producto);
+                return CreatedAtAction(nameof(GetById), new { id = producto.Id }, producto);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e);
+            }
+        } 
+
+        [HttpPut("{id}")] 
+        public async Task<IActionResult> Update(int id, Product producto) 
+        {
+            try
+            {
+                if (id != producto.Id) return BadRequest();
+                await _repository.UpdateAsync(producto);
+                return NoContent();
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e);
+            }
+        } 
+    
+        [HttpDelete("{id}")] 
+        public async Task<IActionResult> Delete(int id)
+        {
+            try
+            {
+                await _repository.DeleteAsync(id);
+                return NoContent();
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e);
+            }
+            
+        } 
+    }
+}
