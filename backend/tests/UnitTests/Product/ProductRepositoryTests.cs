be75d6d92d1b95ab409edab471763644eecfed03
Luis Hernandez <luxo997@gmail.com>
2025-03-16 14:48:46 -0500

    Add: config unittest and first unittest

---
+++ backend/tests/UnitTests/Product/ProductRepositoryTests.cs
@@ -0,0 +1,35 @@
+﻿using Domain.Models;
+using Infrastructure.Persistence;
+using Infrastructure.Persistence.Respositories;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using System;
+using UnitTests.Mock;
+
+namespace UnitTests.ProductRepository;
+
+public class ProductoRepositoryTests {
+
+    private readonly Mock<AplicationDbContext> _mockContext;
+    private readonly ProductoRepository _repository; 
+    private ProductMock _productMock;
+    
+
+    public ProductoRepositoryTests()
+    {
+        _mockContext = new Mock<AplicationDbContext>();
+        _productMock = new ProductMock(_mockContext);
+        _repository = new ProductoRepository(_mockContext.Object);
+    }
+
+
+    [Fact] 
+    public async Task GetAll_ShouldReturnAllProductos() 
+    { 
+
+        var result = await _repository.GetAll(); 
+        
+        Assert.Equal(1, result.Count); 
+    }
+}
+
