4aa67bc3565600e41bab61ab541458fcb7c545fb
Luis Hernández <luxo997@gmail.com>
2025-03-17 04:17:12 -0500

    Update: Link

--- frontend/src/components/ErrorMessage.tsx
+++ frontend/src/components/ErrorMessage.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router';
 
 interface ErrorMessageInterface {
   title: string;
@@ -22,7 +23,7 @@ const ErrorMessage: React.FC<ErrorMessageInterface> = (props) => {
     <div className="flex h-screen flex-col bg-white dark:bg-gray-900">
       <img
         src="https://images.unsplash.com/photo-1558769132-cb1aea458c5e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1548&q=80"
-        alt=""
+        alt="imagen error"
         className="h-64 w-full object-cover"
       />
 
@@ -36,12 +37,12 @@ const ErrorMessage: React.FC<ErrorMessageInterface> = (props) => {
             {description}
           </p>
 
-          <a
-            href="#"
+          <Link
+            to="/"
             className="mt-6 inline-block rounded-sm bg-indigo-600 px-5 py-3 text-sm font-medium text-white hover:bg-indigo-700 focus:ring-3 focus:outline-hidden"
           >
-            Go Back Home
-          </a>
+            Ir al inicio
+          </Link>
         </div>
       </div>
     </div>
