9a42ff7ee4497c6db7cd07054285bee6c13ac1ff
Luis Hernández <luxo997@gmail.com>
2025-03-16 21:34:16 -0500

    Update: new routes product detail, addproduct, edit

--- frontend/src/routes/router.tsx
+++ frontend/src/routes/router.tsx
@@ -1,11 +1,27 @@
 import { useRoutes } from "react-router";
+
 import Home from "@pages/Home";
+import Product from "@pages/Product";
+import AddProduct from "@pages/AddProduct";
 
 const Router = () => {
   let element = useRoutes([
     {
       path: "/",
       element: <Home />
+    },
+    {
+      path: "/ProductDetail/:id",
+      element: <Product />
+    },
+    {
+      path: "/AddProduct",
+      element: <AddProduct />
+    },
+    
+    {
+      path: "/EditProduct/:id",
+      element: <AddProduct />
     },
   ]);
 
