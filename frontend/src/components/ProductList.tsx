5b656319d5b94b7013b46e94ff50804e3223bd80
Luis Hernández <luxo997@gmail.com>
2025-03-16 19:30:11 -0500

    Update: integration request with queries

--- frontend/src/components/ProductList.tsx
+++ frontend/src/components/ProductList.tsx
@@ -1,11 +1,17 @@
-
+import { useProducts } from "../api/queries";
+import ErrorMessage from "./ErrorMessage";
+import Loading from "./Loading";
 import ProductCard from "./ProductCard";
 
 const ProductList = () => {
+  const { data: products, isLoading, isError } = useProducts();
+
+  if (isLoading) return <Loading />;
+  if (isError) return <ErrorMessage title="Bad Request" description="Error al cargar productos" />;
 
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-      {[1,2,3,4,1,23,4]?.map((product) => (
+      {products?.map((product) => (
         <ProductCard key={product.id} product={product} />
       ))}
     </div>
