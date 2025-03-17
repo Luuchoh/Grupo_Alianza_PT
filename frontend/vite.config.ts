30667443fdcb0da13d83e25c995b34ec071e2c3d
Luis Hernández <luxo997@gmail.com>
2025-03-16 19:47:21 -0500

    Add: LayoutBase and alias

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -18,6 +18,7 @@ export default defineConfig({
       "@hooks" :`${path.resolve(__dirname, "src/hooks" )}`,
       "@pages" :`${path.resolve(__dirname, "src/pages" )}`,
       "@routes" :`${path.resolve(__dirname, "src/routes" )}`,
+      "@layouts" :`${path.resolve(__dirname, "src/layouts" )}`,
     }
   }
 })
